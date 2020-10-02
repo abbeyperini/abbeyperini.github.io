b956ad7abadf13840fc295e112549c0561eabc9c
abbeyperini <abbeyperini@gmail.com>
2020-10-02 15:02:54 -0400

    slideshow galleries

--- Scripts/main.js
+++ Scripts/main.js
@@ -1,31 +1,46 @@
 let myImage = document.getElementById('this_image');
 
-myImage.addEventListener("click", function() {
-    let mySrc = myImage.getAttribute('src');
-    if(mySrc === 'Images/laceshawl.jpg') {
-      myImage.setAttribute ('src','Images/rainbow-scarf.jpg');
-      myImage.setAttribute ('alt','Subtle Interruption Shawl by Espace Tricot');
-    } 
-    else if(mySrc === 'Images/rainbow-scarf.jpg') {
-      myImage.setAttribute ('src', 'Images/nezuko.jpg');
-      myImage.setAttribute ('alt', 'Abbey in a Nezuko Kamado costume - picture by Patrick Sun');
-    }
-    else if(mySrc === 'Images/nezuko.jpg') {
-      myImage.setAttribute ('src', 'Images/violet.jpg');
-      myImage.setAttribute ('alt', 'Abbey in a Violet Evergarden costume');
-    }
-    else if(mySrc === 'Images/violet.jpg') {
-      myImage.setAttribute ('src', "Images/rings-of-change.jpg");
-      myImage.setAttribute ('alt', "A crochet blanket in teal yarn made following the pattern Rings of Change by Frank O'Randle");
-    }
-    else if(mySrc === 'Images/rings-of-change.jpg') {
-      myImage.setAttribute ('src', "Images/GLOW.jpg");
-      myImage.setAttribute ('alt', "Abbey dressed as Liberty Belle from Netflix's GLOW");
-    }
-    else {
-      myImage.setAttribute ('src','Images/laceshawl.jpg');
-      myImage.setAttribute ('alt', "A knit lace shawl in burgundy sparkly yarn made following the pattern Dowland by Dee O'Keefe");
-    }
-})
+let sources = ['Images/rainbow-scarf.jpg', 'Images/nezuko.jpg', 
+  'Images/violet.jpg', "Images/rings-of-change.jpg", "Images/GLOW.jpg", 'Images/laceshawl.jpg'];
+let alts = ['Subtle Interruption Shawl by Espace Tricot', 'Abbey in a Nezuko Kamado costume - picture by Patrick Sun', 
+  'Abbey in a Violet Evergarden costume', "A crochet blanket in teal yarn made following the pattern Rings of Change by Frank O'Randle", 
+  "Abbey dressed as Liberty Belle from Netflix's GLOW", "A knit lace shawl in burgundy sparkly yarn made following the pattern Dowland by Dee O'Keefe"];
+
+let faSources = ['Images/rainbow-scarf.jpg', "Images/swoctopus.jpg", "Images/rings-of-change.jpg", 'Images/laceshawl.jpg', 
+  'Images/pineappleHat.jpg', 'Images/ami-elephants.jpg']
+let faAlts = ["A knit shawl made in rainbow and silver/grey yarn following the patter Subtle Interruption Shawl by Espace Tricot",
+  "A knit sweater in white and shades of blue and purple made following the pattern Embrace Octopus Sweater by Maia E. Sirnes",
+  "A crochet blanket in teal yarn made following the pattern Rings of Change by Frank O'Randle",
+  "A knit lace shawl in burgundy sparkly yarn made following the pattern Dowland by Dee O'Keefe",
+  'A knit hat designed to look like a pinapple following the pattern Pineapple Hat by Rachel M. Hansen',
+  "Two stuffed crochet Amigurumi Elephants in pink and grey - pattern from allaboutami.com"];
+
+let cosSources = ['Images/violet.jpg', 'Images/nezuko.jpg', 'Images/sailor-moon.jpg', 'Images/overhaul.jpg',
+'Images/GLOW.jpg', 'Images/goodOmens.jpg', 'Images/BB.jpg'];
+let cosAlts = ['Abbey dressed as Violet Evergarden', 'Abbey in a Nezuko Kamado costume - picture by Patrick Sun',
+  'Abbey dressed as Sailor Moon - photo by Chris Wold', 'Abbey dressed as Overhaul from My Hero Academia', "Abbey dressed as Liberty Belle from Netflix's GLOW",
+  'Abbey dressed as Aziraphale from Good Omens', "Abbey dressed as Bombshell Batwoman - Away Game Variant"];
+
+let counter = 0;
+
+window.setInterval(() => {
+  let sourceArray = sources;
+  let altArray = alts;
+
+  if (document.URL.includes("cosplay.html")) {
+    sourceArray = cosSources;
+    altArray = cosAlts;
+  } else if (document.URL.includes("fiber-arts.html")) {
+    sourceArray = faSources;
+    altArray = faAlts;};
+
+  myImage.setAttribute ('src', sourceArray[counter]);
+  myImage.setAttribute ('alt', altArray[counter]);
+
+  if (counter > sources.length - 2) {
+    counter = 0;
+  } else {
+    counter++; };
+}, 2000);
 
 /* add code to keep copyright date current */
